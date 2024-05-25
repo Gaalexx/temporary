481206b75f2a7947a2f8c092892e220eec2b2803
myar <yaroslav.markelov.09@mail.ru>
2024-05-25 18:44:12 +0300

    Revert "qweq"
    This reverts commit 8006f04ffd49c85fe700a01ab840dd281d00c714.

--- GIGAchat/func_config_parser.py
+++
@@ -1,7 +0,0 @@
-import configparser
-
-
-def get_auth() -> str:
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    return config["GIGACHAT"]["auth"]
--- func/calendar_func.py
+++ func/calendar_func.py
@@ -37,17 +37,16 @@ def next_day_link(year: int, month: int, day: int) -> str:
     next_day = datetime.date(year, month, day) + datetime.timedelta(days=1)
     return f"/{next_day.year}/{next_day.month}/{next_day.day}"
 
-
 def next_day(year, month, day):
     next_day = datetime.date(int(year), int(month), int(day)) + datetime.timedelta(days=1)
     return [str(next_day.year), str(next_day.month), str(next_day.day)]
-
 
 def next_week(year, month, day):
     next_week = datetime.date(int(year), int(month), int(day)) + datetime.timedelta(days=7)
     return [str(next_week.year), str(next_week.month), str(next_week.day)]
 
-
 def next_year(year, month, day):
     next_week = datetime.date(int(year), int(month), int(day)) + datetime.timedelta(days=365)
     return [str(next_week.year), str(next_week.month), str(next_week.day)]
+
+
